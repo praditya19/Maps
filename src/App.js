317a85c76846bda7dde2957b6f574c177c6d4cff
praditya <praditya.rendi88@gmail.com>
2019-07-24 11:55:27 +0700

    membuat maps

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Axios from "axios";
 
 class App extends Component {
+  componentDidMount() {
+    this.getVenues();
+    this.renderMap();
+  }
+
+  renderMap = () => {
+    loadScript(
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBwNIPdWHUSaCTo0S4auHHRd_1WBdLiI6k&callback=initMap"
+    );
+    window.initMap = this.initMap;
+  };
+
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore";
+    const parameters = {
+      client_id: "MH3QUI3MB2ICJGR2PLI4I0K2QMQOJVF5LCERZU3W4S4JSWUA",
+      client_secret: "SWX1KVDFHPVQLEKPAPAX1Q3HEEFJ0DVL0VDIOQRQCCZAIGEZ",
+      query: "food",
+      near: "Sydne",
+      v: "20182507"
+    };
+
+    Axios.get(endPoint + new URLSearchParams(parameters))
+      .then(res => {
+        console.log(res);
+      })
+      .catch(a => {
+        console.log(a);
+      });
+  };
+
+  initMap = () => {
+    var map = new window.google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main>
+        <div id="map" />
+      </main>
     );
   }
 }
 
+function loadScript(url) {
+  var index = window.document.getElementsByTagName("script")[0];
+  var script = window.document.createElement("script");
+  script.src = url;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index);
+}
 export default App;
